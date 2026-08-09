5158375bb3b1f0d7987cd0d059c741c4fc057112
Khemendra Kumar <kumarkhemendra2001@gmail.com>
2026-08-09 23:53:49 +0530

    this

---
+++ DOM/DOM/Day68/strictMode/aap.js
@@ -0,0 +1,20 @@
+//strict mode 
+"use strict " // enable strict mode on whole aap.js file
+
+// let a = 10
+// function sum(a, a){
+//     console.log(a+a)
+// }
+// sum(1,99)
+
+// let obj = {
+//     name : "qwerty"
+// }
+// let age = 10
+// delete(obj.name)
+// delete(age)
+
+
+
+let interface = "xyz"
+console.log(interface)
